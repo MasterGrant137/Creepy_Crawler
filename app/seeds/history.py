0f8424d045ffc14b351d48143f9482a80675e73e
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-23 03:24:55 -0700

    cleaning code

--- app/seeds/history.py
+++ app/seeds/history.py
@@ -23,10 +23,3 @@ def seed_users():
 def undo_history():
     db.session.execute('TRUNCATE users RESTART IDENTITY CASCADE;')
     db.session.commit()
-
-
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
-    search = db.Column(db.String(1000), nullable=True)
-    visit = db.Column(db.String(1000), nullable=True)
-    timezone=db.Column(db.String(50), nullable=False)
-    updated_at = db.Column(db.DateTime, nullable=False)
