b0c2fa570f5392e7ee409960b8abb48535deb919
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-23 21:41:23 -0700

    Trying to create a data structure to handle history.

--- react-app/src/components/History_Page/History_Page.js
+++ react-app/src/components/History_Page/History_Page.js
@@ -5,6 +5,10 @@ import '../Main.css';
 import './History_Page.css';
 
 export const HistoryPage = () => {
+    const dispatch = useDispatch();
+    
+    const dayQueue = [null, null];
+
     const dateRegex = new RegExp([
                                 '([A-Z]{1}[a-z]{2}),\\s', //? day of the week
                                 '(\\d{2}\\s[A-Z]{1}[a-z]{2}\\s\\d{4})\\s', //? day, month, and year
@@ -12,9 +16,11 @@ export const HistoryPage = () => {
                                 '(.*)' //? timezone
                                 ].join(''), 'g');
 
-    const abbrevTZRegex = /([A-Z]){1}\w+|(\s)/g
+    const abbrevTZRegex = /([A-Z]){1}[-]?[a-z]+\s/g
 
-    const dispatch = useDispatch();
+    const queueHandler = (dayOfWk, entryID) => {
+        dayQueue.push('acorn')
+    }
 
     useEffect(() => {
         dispatch(readHistoryEntries())
@@ -23,11 +29,20 @@ export const HistoryPage = () => {
     const entriesObj = useSelector(state => state.history);
     const entries = Object.values(entriesObj).map(entry => (
         <div key={entry.id}>
-            <span>{entry['updated_at'].replace(dateRegex, '$1')}</span>
             <span>
-                {function tz() {
+                 {function () {
+                     const dayOfWk = entry['updated_at'].replace(dateRegex, '$1');
+                     queueHandler(dayOfWk, entry.id)
+
+                     return;
+                 }()}
+            </span>
+            <span>
+                {function () {
                     const abbrevTZ = entry.timezone.replace(dateRegex, '$1').replace(abbrevTZRegex, '$1');
                     const natoTZ = /[A-Z]TZ/;
+
+                    queueHandler()
                     return !natoTZ.test(abbrevTZ) ? abbrevTZ : abbrevTZ[0];
                 }()}
             </span>
