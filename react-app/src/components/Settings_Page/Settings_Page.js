2e70315308107e6ff09e06c49dedea8b489e7f02
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-26 05:17:14 -0700

    Phase one of Settings Page functionality complete

--- react-app/src/components/Settings_Page/Settings_Page.js
+++ react-app/src/components/Settings_Page/Settings_Page.js
@@ -4,22 +4,27 @@ import './Settings_Page.css';
 import dropdownData from './dropdown_data.json';
 
 export const SettingsPage = () => {
-    const [dropdown, setDropdown] = useState('invisible');
+    const [fSDropdown, setFSDropdown] = useState('invisible');
+    const [fDropdown, setFDropdown] = useState('invisible');
 
     const fontSizes = dropdownData['font-sizes'];
     const fonts = dropdownData['fonts'];
 
 
-    const dropdownHandler = (eType, e) => {
-        if (eType === 'onMouseOver') setDropdown('dropdown-1');
-        else setDropdown('invisible');
+    const dropdownHandler = (eType, eTarg, e) => {
+        if (eType === 'onMouseOver') {
+           eTarg === 'FS' ? setFSDropdown('dropdown-1') : setFDropdown('dropdown-1');
+        }
+        else {
+            eTarg === 'FS' ? setFSDropdown('invisible') : setFDropdown('invisible');
+        };
     }
 
     const fontSizeChoices = fontSizes.map(fontSize => (
-        <div key={fontSize} className={dropdown}>{fontSize}</div>
+        <div key={fontSize} className={fSDropdown}>{fontSize}</div>
     ))
     const fontChoices = fonts.map(font => (
-        <div key={font} className={dropdown}>{font}</div>
+        <div key={font} className={fDropdown}>{font}</div>
     ))
 
     return (
@@ -28,24 +33,37 @@ export const SettingsPage = () => {
             <div>
                 <h2>Customize Theme</h2>
                 <form>
-                    <label htmlFor='bg-color-picker'>Background Color</label>
+                    <div>
+                        <label htmlFor='sett-pg-bg-color-picker'>Background Color</label>
                         <input
                             type='color'
-                            name='bg-color-picker'
+                            id='sett-pg-bg-color-picker'
                         />
-                    <label htmlFor='font-color-picker'>Font Color</label>
+                    </div>
+                    <div>
+                        <label htmlFor='sett-pg-font-color-picker'>Font Color</label>
                         <input
                             type='color'
-                            name='font-color-picker'
+                            id='sett-pg-font-color-picker'
                         />
-                    <div className='dropdown-1-container' onMouseOver={(e) => dropdownHandler('onMouseOver', e)} onMouseOut={(e) => dropdownHandler('onMouseOut', e)}>
+                    </div>
+                    <div 
+                        className='dropdown-1-container'
+                        onMouseOver={(e) => dropdownHandler('onMouseOver', 'FS', e)}
+                        onMouseOut={(e) => dropdownHandler('onMouseOut', 'FS', e)}
+                    >
                         <span>Font Size</span>
                         {fontSizeChoices}
                     </div>
-                    <div className='dropdown-1-container' onMouseOver={(e) => dropdownHandler('onMouseOver', e)} onMouseOut={(e) => dropdownHandler('onMouseOut', e)}>
+                    <div 
+                        className='dropdown-1-container'
+                        onMouseOver={(e) => dropdownHandler('onMouseOver', 'S', e)}
+                        onMouseOut={(e) => dropdownHandler('onMouseOut', 'S', e)}
+                    >
                         <span>Font</span>
                         {fontChoices}
                     </div>
+                    <button>Submit</button>
                 </form>
             </div>
         </div>
