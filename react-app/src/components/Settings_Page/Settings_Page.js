cdeb021171aac3ed9b23478f74f5fe554af1d766
Alejandro C. Grant <authorcgrant@gmail.com>
2021-10-30 16:33:10 -0700

    Assuring the cancel button applies to the create theme section as well

--- react-app/src/components/Settings_Page/Settings_Page.js
+++ react-app/src/components/Settings_Page/Settings_Page.js
@@ -47,32 +47,44 @@ export const SettingsPage = ({ style }) => {
 
     const resetHandler = (e, formType) => {
         const targID = e.target.dataset.settingId;
-  
         let targForm;
-
+        
         if (formType === 'editor') {
             targForm = document.getElementById(`sett-pg-editor-form-${targID}`);
+            const prev = settingsObj[targID];
+            const targFormKids = Array.from(targForm.children);
+            targFormKids.forEach(targKid => {
+                switch (targKid.dataset.inputName) {
+                    case 'Theme Name': targKid.value = prev.theme_name; break;
+                    case 'Background Color': targKid.value = prev.background_color; break;
+                    case 'Background Rotate': targKid.checked = prev.background_rotate; break;
+                    case 'Font Color': targKid.value = prev.font_color; break;
+                    case 'Font Family': targKid.value = prev.font_family.replace(/,\s/, ' | '); break;
+                    case 'Font Size': targKid.value = prev.font_size.replace('px', ''); break;
+                    case 'Accent 1': targKid.value = prev.accent_1; break;
+                    case 'Accent 2': targKid.value = prev.accent_2; break;
+                    case 'Accent 3': targKid.value = prev.accent_3; break;
+                    default: break;
+                }
+            })
         } else if (formType === 'picker') {
             targForm = document.getElementById(`sett-pg-picker-form-${targID}`);
+            const targFormKids = Array.from(targForm.children);
+            targFormKids.forEach(targKid => {
+                switch (targKid.dataset.inputName) {
+                    case 'Theme Name': setThemeName(style.theme_name || 'theme'); break;
+                    case 'Background Color': setBackgroundColor(style.background_color); break;
+                    case 'Background Rotate': setBackgroundRotate(style.background_rotate); break;
+                    case 'Font Color': setFontColor(style.font_color); break;
+                    case 'Font Family': setFontFamily(style.font_family.replace(/,\s/, ' | ')); break;
+                    case 'Font Size': setFontSize(style.font_size.replace('px', '')); break;
+                    case 'Accent 1': setAccent1(style.accent_1); break;
+                    case 'Accent 2': setAccent2(style.accent_2); break;
+                    case 'Accent 3': setAccent3(style.accent_3); break;
+                    default: break;
+                }
+            })
         }
-
-        const targFormKids = Array.from(targForm.children);
-        const prev = settingsObj[targID];
-
-        targFormKids.forEach(targKid => {
-            switch (targKid.dataset.inputName) {
-                case 'Theme Name': targKid.value = prev.theme_name; break;
-                case 'Background Color': targKid.value = prev.background_color; break;
-                case 'Background Rotate': targKid.checked = prev.background_rotate; break;
-                case 'Font Color': targKid.value = prev.font_color; break;
-                case 'Font Family': targKid.value = prev.font_family.replace(/,\s/, ' | '); break;
-                case 'Font Size': targKid.value = prev.font_size.replace('px', ''); break;
-                case 'Accent 1': targKid.value = prev.accent_1; break;
-                case 'Accent 2': targKid.value = prev.accent_2; break;
-                case 'Accent 3': targKid.value = prev.accent_3; break;
-                default: break;
-            }
-        })
     }
 
     const setProfileMediaHandler = (e) => {
@@ -344,127 +356,109 @@ export const SettingsPage = ({ style }) => {
             <div>
                 <h2>Create Theme</h2>
                 <form id='sett-pg-picker-form-2' onSubmit={createSettingHandler}>
-                    <div>
-                        <label htmlFor='sett-page-theme-name-picker'>Theme Name</label>
-                        <input
-                            id='sett-page-theme-name-picker'
-                            readOnly={p_f_2_disabled}
-                            data-input-name={'Theme Name'}
-                            type='text'
-                            placeholder='Theme Name'
-                            value={theme_name}
-                            onChange={(e) => setThemeName(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='sett-pg-bg-color-picker'>Background Color</label>
-                        <input
-                            id='sett-pg-bg-color-picker'
-                            data-input-name={'Background Color'}
-                            type='color'
+                    <label htmlFor='sett-page-theme-name-picker'>Theme Name</label>
+                    <input
+                        id='sett-page-theme-name-picker'
+                        readOnly={p_f_2_disabled}
+                        data-input-name={'Theme Name'}
+                        type='text'
+                        placeholder='Theme Name'
+                        value={theme_name}
+                        onChange={(e) => setThemeName(e.target.value)}
+                    />
+                    <label htmlFor='sett-pg-bg-color-picker'>Background Color</label>
+                    <input
+                        id='sett-pg-bg-color-picker'
+                        data-input-name={'Background Color'}
+                        type='color'
+                        disabled={p_f_2_disabled}
+                        value={background_color}
+                        onChange={(e) => setBackgroundColor(e.target.value)}
+                    />
+                <div>
+                    <label htmlFor='s-p-background-media-uploader'>
+                        {background_media !== '' ? 'Background Media' : 'Added'}
+                    </label>
+                    <input
+                        id='s-p-background-media-uploader'
+                        type='file'
+                        disabled={p_f_2_disabled}
+                        onChange={setBackgroundMediaHandler}
+                    />
+                    {background_media_loading && (<span>Loading...</span>)}
+                </div>
+                    <label htmlFor='sett-pg-bg-rotate-picker'>Background Rotate</label>
+                    <input
+                        id='sett-pg-bg-rotate-picker'
+                        data-input-name={'Background Rotate'}
+                        type='checkbox'
+                        disabled={p_f_2_disabled}
+                        checked={background_rotate}
+                        onChange={(e) => setBackgroundRotate(e.target.checked)}
+                    />
+                    <label htmlFor='sett-pg-font-color-picker'>Font Color</label>
+                    <input
+                        id='sett-pg-font-color-picker'
+                        data-input-name={'Font Color'}
+                        type='color'
+                        disabled={p_f_2_disabled}
+                        value={font_color}
+                        onChange={(e) => setFontColor(e.target.value)}
+                    />
+                    <label htmlFor='font-sizes'>Font Size</label>
+                    <select name='font-sizes'
+                            id='sett-pg-font-size-picker'
+                            data-input-name={'Font Size'}
                             disabled={p_f_2_disabled}
-                            value={background_color}
-                            onChange={(e) => setBackgroundColor(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='s-p-background-media-uploader'>
-                            {background_media !== '' ? 'Background Media' : 'Added'}
-                        </label>
-                        <input
-                            id='s-p-background-media-uploader'
-                            type='file'
-                            disabled={p_f_2_disabled}
-                            onChange={setBackgroundMediaHandler}
-                        />
-                        {background_media_loading && (<span>Loading...</span>)}
-                    </div>
-                    <div>
-                        <label htmlFor='sett-pg-bg-rotate-picker'>Background Rotate</label>
-                        <input
-                            id='sett-pg-bg-rotate-picker'
-                            data-input-name={'Background Rotate'}
-                            type='checkbox'
-                            disabled={p_f_2_disabled}
-                            checked={background_rotate}
-                            onChange={(e) => setBackgroundRotate(e.target.checked)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='sett-pg-font-color-picker'>Font Color</label>
-                        <input
-                            id='sett-pg-font-color-picker'
-                            data-input-name={'Font Color'}
-                            type='color'
-                            disabled={p_f_2_disabled}
-                            value={font_color}
-                            onChange={(e) => setFontColor(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='font-sizes'>Font Size</label>
-                        <select name='font-sizes'
-                                id='sett-pg-font-size-picker'
-                                data-input-name={'Font Size'}
-                                disabled={p_f_2_disabled}
-                                value={font_size?.replace('px', '')}
-                                onChange={(e) => {
-                                    const targOption = Array.from(e.target.children).find(option => option.selected);
-                                    setFontSize(`${targOption.innerText}px`);
-                                }}
-                        >
-                            {fontSizes}
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor='font-families'>Font Family</label>
-                        <select 
-                            name='font-families'
-                            id='sett-pg-font-family-picker'
-                            data-input-name={'Font Family'}
-                            disabled={p_f_2_disabled}
-                            value={font_family?.replace(/,\s/, ' | ')}
+                            value={font_size?.replace('px', '')}
                             onChange={(e) => {
                                 const targOption = Array.from(e.target.children).find(option => option.selected);
-                                setFontFamily(targOption.innerText.replace(/\s\|\s/, ', '));
+                                setFontSize(`${targOption.innerText}px`);
                             }}
-                        >
-                            {fontFamilies}
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor='sett-pg-accent-1-color-picker'>Accent 1</label>
-                        <input
-                            id='sett-pg-accent-1-color-picker'
-                            data-input-name={'Accent 1'}
-                            type='color'
-                            disabled={p_f_2_disabled}
-                            value={accent_1}
-                            onChange={(e) => setAccent1(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='sett-pg-accent-2-color-picker'>Accent 2</label>
-                        <input
-                            id='sett-pg-accent-2-color-picker'
-                            data-input-name={'Accent 2'}
-                            type='color'
-                            disabled={p_f_2_disabled}
-                            value={accent_2}
-                            onChange={(e) => setAccent2(e.target.value)}
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor='sett-pg-accent-3-color-picker'>Accent 3</label>
-                        <input
-                            id='sett-pg-accent-3-color-picker'
-                            data-input-name={'Accent 3'}
-                            type='color'
-                            disabled={p_f_2_disabled}
-                            value={accent_3}
-                            onChange={(e) => setAccent3(e.target.value)}
-                        />
-                    </div>
+                    >
+                        {fontSizes}
+                    </select>
+                    <label htmlFor='font-families'>Font Family</label>
+                    <select 
+                        name='font-families'
+                        id='sett-pg-font-family-picker'
+                        data-input-name={'Font Family'}
+                        disabled={p_f_2_disabled}
+                        value={font_family?.replace(/,\s/, ' | ')}
+                        onChange={(e) => {
+                            const targOption = Array.from(e.target.children).find(option => option.selected);
+                            setFontFamily(targOption.innerText.replace(/\s\|\s/, ', '));
+                        }}
+                    >
+                        {fontFamilies}
+                    </select>
+                    <label htmlFor='sett-pg-accent-1-color-picker'>Accent 1</label>
+                    <input
+                        id='sett-pg-accent-1-color-picker'
+                        data-input-name={'Accent 1'}
+                        type='color'
+                        disabled={p_f_2_disabled}
+                        value={accent_1}
+                        onChange={(e) => setAccent1(e.target.value)}
+                    />
+                    <label htmlFor='sett-pg-accent-2-color-picker'>Accent 2</label>
+                    <input
+                        id='sett-pg-accent-2-color-picker'
+                        data-input-name={'Accent 2'}
+                        type='color'
+                        disabled={p_f_2_disabled}
+                        value={accent_2}
+                        onChange={(e) => setAccent2(e.target.value)}
+                    />
+                    <label htmlFor='sett-pg-accent-3-color-picker'>Accent 3</label>
+                    <input
+                        id='sett-pg-accent-3-color-picker'
+                        data-input-name={'Accent 3'}
+                        type='color'
+                        disabled={p_f_2_disabled}
+                        value={accent_3}
+                        onChange={(e) => setAccent3(e.target.value)}
+                    />
                     <button>{p_f_2_btn}</button>
                     <button type='button' data-setting-id={2} onClick={(e) => resetHandler(e, 'picker')}>Cancel</button>
                 </form>
@@ -472,9 +466,10 @@ export const SettingsPage = ({ style }) => {
                     border: `5px solid ${smpl.a_3}`,
                     backgroundColor: smpl.b_c,
                     fontFamily: smpl.f_f,
-                    fontSize: smpl.f_s
+                    fontSize: smpl.f_s,
+                    color: smpl.f_c
                 }}>
-                    <h2 style={{color: smpl.f_c}}>{`${theme_name}` || 'Theme Name'}</h2>
+                    <h2>{`${theme_name}` || 'Theme Name'}</h2>
                     <div style={{
                         borderTop: `5px solid ${smpl.a_1}`,
                         backgroundColor: smpl.b_c
