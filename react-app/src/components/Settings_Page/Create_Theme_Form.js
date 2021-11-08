127b8bf1f49039185fcb9a736e1180c4bdce4e5c
Alejandro C. Grant <authorcgrant@gmail.com>
2021-11-08 15:27:33 -0800

    cutting through the create form cleaning

--- react-app/src/components/Settings_Page/Create_Theme_Form.js
+++ react-app/src/components/Settings_Page/Create_Theme_Form.js
@@ -79,8 +79,6 @@ export const CreateThemeForm = ({ style }) => {
     return (
         <>
             <form 
-                id='sett-pg-setter-form-2'
-                data-setting-id={2}
                 className='setter-form-2' 
                 onSubmit={createSettingHandler}
                 style={{ border: `3px solid ${style.accent_3}`, borderRight: '0' }}
@@ -111,10 +109,10 @@ export const CreateThemeForm = ({ style }) => {
                 </button>
                 <div className='sett-pg-row-a'>
                     <div className='theme-name-setter-div'>
-                        <label data-label-name='Theme Name' htmlFor='sett-pg-theme-name-setter'>Theme Name</label>
+                        <label htmlFor='theme-name-setter'>Theme Name</label>
                         <input
-                            id='sett-pg-theme-name-setter'
-                            data-input-name='Theme Name'
+                            id='theme-name-setter'
+                            name='Theme Name'
                             type='text'
                             placeholder='Theme Name'
                             value={theme_name}
@@ -123,11 +121,10 @@ export const CreateThemeForm = ({ style }) => {
                         />
                     </div>
                     <div className='font-size-setter-div'>
-                        <label data-label-name='Font Size' htmlFor='sett-pg-font-size-setter'>Font Size</label>
+                        <label htmlFor='font-size-setter'>Font Size</label>
                         <select 
-                            id='sett-pg-font-size-setter'
-                            name='sett-pg-font-size-setter'
-                            data-input-name='Font Size'
+                            id='font-size-setter'
+                            name='Font Size'
                             value={font_size?.replace('px', '')}
                             onChange={(e) => {
                                 const targOption = Array.from(e.target.children).find(option => option.selected);
@@ -138,11 +135,10 @@ export const CreateThemeForm = ({ style }) => {
                         </select>
                     </div>
                     <div className='font-family-setter-div'>
-                        <label data-label-name='Font Family' htmlFor='sett-pg-font-family-setter'>Font Family</label>
+                        <label htmlFor='font-family-setter'>Font Family</label>
                         <select 
-                            id='sett-pg-font-family-setter'
-                            name='sett-pg-font-family-setter'
-                            data-input-name='Font Family'
+                            id='font-family-setter'
+                            name='Font Family'
                             value={font_family?.replace(/,\s/, ' | ')}
                             onChange={(e) => {
                                 const targOption = Array.from(e.target.children).find(option => option.selected);
@@ -155,50 +151,50 @@ export const CreateThemeForm = ({ style }) => {
                 </div>
                 <div className='sett-pg-row-b'>
                     <div className='font-color-setter-div'>
-                        <label data-label-name='Font Color' htmlFor='sett-pg-font-color-setter'>Font Color</label>
+                        <label htmlFor='sett-pg-font-color-setter'>Font Color</label>
                         <input
                             id='sett-pg-font-color-setter'
-                            data-input-name='Font Color'
+                            name='Font Color'
                             type='color'
                             value={font_color}
                             onChange={(e) => setFontColor(e.target.value)}
                         />
                     </div>
                     <div className='accent-1-setter-div'>
-                        <label data-label-name='Accent 1' htmlFor='sett-pg-accent-1-color-setter'>Accent 1</label>
+                        <label htmlFor='sett-pg-accent-1-color-setter'>Accent 1</label>
                         <input
                             id='sett-pg-accent-1-color-setter'
-                            data-input-name='Accent 1'
+                            name='Accent 1'
                             type='color'
                             value={accent_1}
                             onChange={(e) => setAccent1(e.target.value)}
                         />
                     </div>
                     <div className='accent-2-setter-div'>
-                        <label data-label-name='Accent 2' htmlFor='sett-pg-accent-2-color-setter'>Accent 2</label>
+                        <label htmlFor='sett-pg-accent-2-color-setter'>Accent 2</label>
                         <input
                             id='sett-pg-accent-2-color-setter'
-                            data-input-name='Accent 2'
+                            name='Accent 2'
                             type='color'
                             value={accent_2}
                             onChange={(e) => setAccent2(e.target.value)}
                         />
                     </div>
                     <div className='accent-3-setter-div'>
-                        <label data-label-name='Accent 3' htmlFor='sett-pg-accent-3-color-setter'>Accent 3</label>
+                        <label htmlFor='sett-pg-accent-3-color-setter'>Accent 3</label>
                         <input
                             id='sett-pg-accent-3-color-setter'
-                            data-input-name='Accent 3'
+                            name='Accent 3'
                             type='color'
                             value={accent_3}
                             onChange={(e) => setAccent3(e.target.value)}
                         />
                     </div>
                     <div className='background-color-setter-div'>
-                        <label data-label-name='Background Color' htmlFor='sett-pg-bg-color-setter'>Background Color</label>
+                        <label htmlFor='sett-pg-bg-color-setter'>Background Color</label>
                         <input
                             id='sett-pg-bg-color-setter'
-                            data-input-name='Background Color'
+                            name='Background Color'
                             type='color'
                             value={background_color}
                             onChange={(e) => { setBackgroundColor(e.target.value) }}
@@ -207,22 +203,22 @@ export const CreateThemeForm = ({ style }) => {
                 </div>
                 <div className='sett-pg-row-c'>
                     <div className='background-media-setter-div'>
-                        <label data-label-name='Background Media' htmlFor='sett-pg-background-media-uploader'>
+                        <label htmlFor='sett-pg-background-media-uploader'>
                             {background_media !== '' ? 'Background Media' : 'Added'}
                         </label>
                         <input
                             id='sett-pg-background-media-uploader'
-                            data-input-name='Background Media'
+                            name='Background Media'
                             type='file'
                             onChange={setBackgroundMediaHandler}
                         />
                         {background_media_loading && (<span>Loading...</span>)}
                     </div>
                     <div className='background-rotate-setter-div'>
-                        <label data-label-name='Background Rotate' htmlFor='sett-pg-bg-rotate-setter'>Background Rotate</label>
+                        <label htmlFor='sett-pg-bg-rotate-setter'>Background Rotate</label>
                         <input
                             id='sett-pg-bg-rotate-setter'
-                            data-input-name='Background Rotate'
+                            name='Background Rotate'
                             type='checkbox'
                             checked={background_rotate}
                             onChange={(e) => setBackgroundRotate(e.target.checked)}
