e9fdc3516b94920e35c3a54f2125ada85eae7249
Alejandro C. Grant <authorcgrant@gmail.com>
2021-11-19 10:50:59 -0800

    working on getting crawlers functional

--- react-app/src/crawler/crawler/spiders/caerostris_darwini.py
+++ react-app/src/crawler/crawler/spiders/caerostris_darwini.py
@@ -9,42 +9,84 @@ Categories:
 
 import scrapy
 
-
-class CaerostrisDarwiniSpiderCommentarial(scrapy.Spider):
+class CDS(scrapy.Spider):
     """Commentarial spider."""
     
     name = 'caerostris_darwini_commentarial'
-    start_urls = ['http://https://www.espn.com/', 'http://https://www.bbc.com/', 'http://https://www.ign.com/']
+    start_urls = ['https://www.espn.com/']
 
     def parse(self, response):
-        pass
+        for div in response.css('div'):
+            try:
+                if (div.css('p::text').get() == 'Terence Crawford'):
+                    pass
+                yield {
+                    'p': div.css('p::text').get(),
+                    'span': div.css('span::text').get()
+                }
+            except:
+                pass
+
+        # for p in response.css('p::text'):
+        #     yield {
+        #         'p': p.get()
+        #     }
+
+        # for p in response.css('p::text'):
+        #     yield {
+        #         'p': p.get()
+        #     }
+
+# class CDS(scrapy.Spider):
+#     """Commentarial spider."""
+    
+#     name = 'caerostris_darwini_commentarial'
+#     start_urls = ['https://www.espn.com/']
+
+#     def parse(self, response):
+#         for link in response.css('a'):
+#             yield {
+#                 'link': link.attrib['href']
+#             }
+
+# class CaerostrisDarwiniSpiderCommentarial(scrapy.Spider):
+#     """Commentarial spider."""
+    
+#     name = 'caerostris_darwini_commentarial'
+#     start_urls = ['https://www.espn.com/', 'https://www.bbc.com/', 'https://www.ign.com/']
+
+#     def parse(self, response):
+#         for link in response.css('a'):
+#             # yield {
+#             link.attrib['href'].get()
+#             # }
 
 
-class CaerostrisDarwiniSpiderCommercial(scrapy.Spider):
-    """Commercial spider."""
+# class CaerostrisDarwiniSpiderCommercial(scrapy.Spider):
+#     """Commercial spider."""
 
-    name = 'caerostris_darwini_commercial'
-    start_urls = ['http://https://www.walmart.com/', 'http://https://m.yelp.com/']
+#     name = 'caerostris_darwini_commercial'
+#     start_urls = ['https://www.walmart.com/', 'https://m.yelp.com/']
 
-    def parse(self, response):
-        pass
-
-
-class CaerostrisDarwiniSpiderEncyclopedic(scrapy.Spider):
-    """Encyclopedic spider."""
-
-    name = 'caerostris_darwini_encyclopedic'
-    start_urls = ['http://https://www.dictionary.com/', 'http://https://www.nih.gov/', 'http://https://en.m.wikipedia.org/']
-
-    def parse(self, response):
-        pass
+#     def parse(self, response):
+#         pass
 
 
-class CaerostrisDarwiniSpiderVideographic(scrapy.Spider):
-    """Videographic spider."""
+# class CaerostrisDarwiniSpiderEncyclopedic(scrapy.Spider):
+#     """Encyclopedic spider."""
 
-    name = 'caerostris_darwini_videographic'
-    start_urls = ['http://https://m.imdb.com/', 'http://https://m.youtube.com/']
+#     name = 'caerostris_darwini_encyclopedic'
+#     start_urls = ['https://www.dictionary.com/', 'https://www.nih.gov/', 'https://en.m.wikipedia.org/']
 
-    def parse(self, response):
-        pass
+#     def parse(self, response):
+#         pass
+
+
+# class CaerostrisDarwiniSpiderVideographic(scrapy.Spider):
+#     """Videographic spider."""
+
+#     name = 'caerostris_darwini_videographic'
+#     start_urls = ['https://m.imdb.com/', 'https://m.youtube.com/']
+
+#     def parse(self, response):
+#         pass
