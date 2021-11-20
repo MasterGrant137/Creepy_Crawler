160de437cb1ac03a9e973d6c4787b172226e911b
Alejandro C. Grant <authorcgrant@gmail.com>
2021-11-19 16:32:31 -0800

    first sample crawler gaining strength, traversing conditionally

--- react-app/src/crawler/crawler/spiders/caerostris_darwini.py
+++ react-app/src/crawler/crawler/spiders/caerostris_darwini.py
@@ -9,21 +9,7 @@ Categories:
 
 import scrapy
 
-# targ_text = 'NFL'
-
-# class CDS(scrapy.Spider):
-#     """Commentarial spider."""
-    
-#     name = 'caerostris_darwini_commentarial'
-#     start_urls = ['https://www.espn.com/']
-
-#     def parse(self, response):
-#         for link in response.css('a::attr(href)'):
-#             yield {
-#                 'link': targ_text
-#             }
-
-targ_text = 'ESPN'
+targ_text = 'San Diego'
 
 class CDS(scrapy.Spider):
     """Commentarial spider."""
@@ -36,19 +22,25 @@ class CDS(scrapy.Spider):
             yield response.follow(link.get(), callback=self.parse_data)
         
     def parse_data(self, response):
-        # all_text = response.css('*::text')
-        all_text = response.css('h1::text')
+        all_text = response.css('*:not(script)::text')
         try:
             for text in all_text:
-                # if (targ_text in all_text):
-                yield {
-                    'url': f'{response.request.url}: {text.get()}',
-                }
+                if (targ_text in text.get()):
+                    yield {
+                        'url': f'{response.request.url}: {text.get()}',
+                    }
         except:
             print('End of the line error.')
 
 
 
+
+
+
+
+
+# * follows and grabs info on traversed links and compares to target text
+# targ_text = 'ESPN'
 
 # class CDS(scrapy.Spider):
 #     """Commentarial spider."""
@@ -61,14 +53,13 @@ class CDS(scrapy.Spider):
 #             yield response.follow(link.get(), callback=self.parse_data)
         
 #     def parse_data(self, response):
-#         # all_text = response.css('*::text')
-#         all_text = response.css('h1::text')
+#         all_text = response.css('*::text')
 #         try:
 #             for text in all_text:
-#                 # if (targ_text in all_text):
-#                 yield {
-#                     'url': response.request.url,
-#                 }
+#                 if (targ_text in text.get()):
+#                     yield {
+#                         'url': f'{response.request.url}: {text.get()}',
+#                     }
 #         except:
 #             print('End of the line error.')
 
@@ -76,26 +67,7 @@ class CDS(scrapy.Spider):
 
 
 
-
-# class CDS(scrapy.Spider):
-#     """Commentarial spider."""
-    
-#     name = 'caerostris_darwini_commentarial'
-#     start_urls = ['https://www.espn.com/']
-#     targ_string = 'NFL'
-
-#     def parse(self, response):
-#         for div in response.css('div'):
-#             try:
-#                 if (targ_string in div.css('*::text').get()):
-#                     yield {
-#                         'targ': div.css('*::text').get()
-#                     }
-#             except:
-#                 pass
-        
-#         next_url = response.css('a')
-
+# * Matches key word for a given element and displays which element matches that text
 # class CDS(scrapy.Spider):
 #     """Commentarial spider."""
     
@@ -122,78 +94,3 @@ class CDS(scrapy.Spider):
 #                     }
 #             except:
 #                 pass
-
-# class CDS(scrapy.Spider):
-#     """Commentarial spider."""
-    
-#     name = 'caerostris_darwini_commentarial'
-#     start_urls = ['https://www.espn.com/']
-
-#     def parse(self, response):
-#         targ_string = 'consequential conference'
-#         for div in response.css('div'):
-#             try:
-#                 if (targ_string in div.css('a::text').get()):
-#                     yield {
-#                         'a': div.css('a::text').get()
-#                     }
-
-#                 if (targ_string in div.css('p::text').get()):
-#                     yield {
-#                         'p': div.css('p::text').get()
-#                     }
-
-#                 if (targ_string in div.css('span::text').get()):
-#                     yield {
-#                         'span': div.css('span::text').get()
-#                     }
-#             except:
-#                 pass
-# class CDS(scrapy.Spider):
-#     """Commentarial spider."""
-    
-#     name = 'caerostris_darwini_commentarial'
-#     start_urls = ['https://www.espn.com/']
-
-#     def parse(self, response):
-#         targ_string = 'consequential conference'
-#         for div in response.css('div'):
-#             try:
-#                 if (targ_string in div.css('a::text').get()):
-#                     yield {
-#                         'a': div.css('a::text').get()
-#                     }
-
-#                 if (targ_string in div.css('p::text').get()):
-#                     yield {
-#                         'p': div.css('p::text').get()
-#                     }
-
-#                 if (targ_string in div.css('span::text').get()):
-#                     yield {
-#                         'span': div.css('span::text').get()
-#                     }
-#             except:
-#                 pass
-
-        # for p in response.css('p::text'):
-        #     yield {
-        #         'p': p.get()
-        #     }
-
-        # for p in response.css('p::text'):
-        #     yield {
-        #         'p': p.get()
-        #     }
-
-# class CDS(scrapy.Spider):
-#     """Commentarial spider."""
-    
-#     name = 'caerostris_darwini_commentarial'
-#     start_urls = ['https://www.espn.com/']
-
-#     def parse(self, response):
-#         for link in response.css('a'):
-#             yield {
-#                 'link': link.attrib['href']
-#             }
