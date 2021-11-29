9f4d9317b818b95daffcb26a50408bdda57a16a5
Alejandro C. Grant <authorcgrant@gmail.com>
2021-11-29 00:30:40 -0800

    some code cleanup in init and index files

--- app/__init__.py
+++ app/__init__.py
@@ -64,13 +64,6 @@ def inject_csrf_token(response):
         httponly=True)
     return response
 
-# @app.route('/', defaults={'path': ''})
-# @app.route('/<path:path>')
-# def react_root(path):
-#     if path == 'favicon.ico':
-#         return app.send_static_file('favicon.ico')
-#     return app.send_static_file('index.html')
-
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def react_root(path):
