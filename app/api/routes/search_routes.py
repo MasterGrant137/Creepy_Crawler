e104ea408f2339aeaa47a1dd1fefe58ada1988e5
Alejandro C. Grant <authorcgrant@gmail.com>
2021-12-20 15:44:44 -0800

    rigorous se testing

--- app/api/routes/search_routes.py
+++ app/api/routes/search_routes.py
@@ -87,7 +87,7 @@ def add_search_entry():
 def scrape_with_crochet(query):
     """Connect Flask with Scrapy asynchronously."""
     dispatcher.connect(_crawler_result, signal=signals.item_scraped)
-    spiders = [caerostris_darwini.BroadCrawler1, caerostris_darwini.BroadCrawler2, caerostris_darwini.BroadCrawler3, caerostris_darwini.BroadCrawler4, caerostris_darwini.BroadCrawler5, caerostris_darwini.BroadCrawler6, caerostris_darwini.BroadCrawler7, caerostris_darwini.BroadCrawler8, caerostris_darwini.BroadCrawler9]
+    spiders = [caerostris_darwini.BroadCrawlerA]
     [crawl_runner.crawl(spider, query=query) for spider in spiders]
     eventual = crawl_runner.join()
     return eventual
@@ -95,6 +95,7 @@ def scrape_with_crochet(query):
 def _crawler_result(item, response, spider):
     """Typecast each element of crawler's yield into dictionary and append to list."""
     output_data.append(dict(item))
+    print(response, spider)
 
 @search_routes.route('/history/visits/', methods=['POST'])
 def add_visit_entry():
--- app/crawler/spider_lair/spiders/caerostris_darwini.py
+++ app/crawler/spider_lair/spiders/caerostris_darwini.py
@@ -9,6 +9,23 @@ Categories:
 
 import scrapy
 
+class BroadCrawlerA(scrapy.Spider):
+    """Broad crawling spider."""
+
+    name = 'broad_crawler_a'
+    start_urls = ['https://en.m.wikipedia.org/']
+    def parse(self, response):
+        """Follow links."""
+        print(response, 'hit!')
+        try:
+            all_text = response.css('*:not(script):not(style)::text')
+            for text in all_text:
+                if self.query in text.get(): 
+                    yield { 'url': response.request.url, 'text': text.get() }
+        except: print(f'End of the line error for {self.name}.')
+
+        yield from response.follow_all(css='a::attr(href)', callback=self.parse)
+
 class BroadCrawler1(scrapy.Spider):
     """Broad crawling spider."""
 
@@ -20,7 +37,9 @@ class BroadCrawler1(scrapy.Spider):
         try:
             all_text = response.css('*:not(script):not(style)::text')
             for text in all_text:
-                if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
+                if self.query in text.get(): 
+                    yield { 'url': response.request.url, 'text': text.get() }
+                    print(f"{self.name} hit")
         except: print(f'End of the line error for {self.name}.')
 
         yield from response.follow_all(css='a::attr(href)', callback=self.parse)
@@ -36,26 +55,28 @@ class BroadCrawler2(scrapy.Spider):
         try:
             all_text = response.css('*:not(script):not(style)::text')
             for text in all_text:
-                if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
+                if self.query in text.get():
+                     yield { 'url': response.request.url, 'text': text.get() }
+                
         except: print(f'End of the line error for {self.name}.')
 
         yield from response.follow_all(css='a::attr(href)', callback=self.parse)
 
-class BroadCrawler3(scrapy.Spider):
-    """Broad crawling spider."""
+# class BroadCrawler3(scrapy.Spider):
+#     """Broad crawling spider."""
 
-    name = 'broad_crawler_3'
-    start_urls = ['https://thebulletin.org/']
+#     name = 'broad_crawler_3'
+#     start_urls = ['https://thebulletin.org/']
 
-    def parse(self, response):
-        """Follow links."""
-        try:
-            all_text = response.css('*:not(script):not(style)::text')
-            for text in all_text:
-                if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
-        except: print(f'End of the line error for {self.name}.')
+#     def parse(self, response):
+#         """Follow links."""
+#         try:
+#             all_text = response.css('*:not(script):not(style)::text')
+#             for text in all_text:
+#                 if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
+#         except: print(f'End of the line error for {self.name}.')
 
-        yield from response.follow_all(css='a::attr(href)', callback=self.parse)
+#         yield from response.follow_all(css='a::attr(href)', callback=self.parse)
 
 class BroadCrawler4(scrapy.Spider):
     """Broad crawling spider."""
@@ -105,50 +126,34 @@ class BroadCrawler6(scrapy.Spider):
 
         yield from response.follow_all(css='a::attr(href)', callback=self.parse)
 
-class BroadCrawler7(scrapy.Spider):
-    """Broad crawling spider."""
+# class BroadCrawler7(scrapy.Spider):
+#     """Broad crawling spider."""
 
-    name = 'broad_crawler_7'
-    start_urls = ['https://m.imdb.com']
+#     name = 'broad_crawler_7'
+#     start_urls = ['https://m.imdb.com']
 
-    def parse(self, response):
-        """Follow links."""
-        try:
-            all_text = response.css('*:not(script):not(style)::text')
-            for text in all_text:
-                if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
-        except: print(f'End of the line error for {self.name}.')
+#     def parse(self, response):
+#         """Follow links."""
+#         try:
+#             all_text = response.css('*:not(script):not(style)::text')
+#             for text in all_text:
+#                 if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
+#         except: print(f'End of the line error for {self.name}.')
 
-        yield from response.follow_all(css='a::attr(href)', callback=self.parse)
+#         yield from response.follow_all(css='a::attr(href)', callback=self.parse)
 
-class BroadCrawler8(scrapy.Spider):
-    """Broad crawling spider."""
+# class BroadCrawler8(scrapy.Spider):
+#     """Broad crawling spider."""
 
-    name = 'broad_crawler_8'
-    start_urls = ['https://m.imdb.com']
+#     name = 'broad_crawler_8'
+#     start_urls = ['https://www.dictionary.com']
 
-    def parse(self, response):
-        """Follow links."""
-        try:
-            all_text = response.css('*:not(script):not(style)::text')
-            for text in all_text:
-                if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
-        except: print(f'End of the line error for {self.name}.')
+#     def parse(self, response):
+#         """Follow links."""
+#         try:
+#             all_text = response.css('*:not(script):not(style)::text')
+#             for text in all_text:
+#                 if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
+#         except: print(f'End of the line error for {self.name}.')
 
-        yield from response.follow_all(css='a::attr(href)', callback=self.parse)
-
-class BroadCrawler9(scrapy.Spider):
-    """Broad crawling spider."""
-
-    name = 'broad_crawler_9'
-    start_urls = ['https://www.dictionary.com']
-
-    def parse(self, response):
-        """Follow links."""
-        try:
-            all_text = response.css('*:not(script):not(style)::text')
-            for text in all_text:
-                if self.query in text.get(): yield { 'url': response.request.url, 'text': text.get() }
-        except: print(f'End of the line error for {self.name}.')
-
-        yield from response.follow_all(css='a::attr(href)', callback=self.parse)
+#         yield from response.follow_all(css='a::attr(href)', callback=self.parse)
