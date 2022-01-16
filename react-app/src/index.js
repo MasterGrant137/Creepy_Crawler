d658cebe82d3abb04a9b6ff242300d1d4770b19a
Alejandro C. Grant <authorcgrant@gmail.com>
2022-01-16 14:02:57 -0800

    file deletion

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import App from './App';
 import configureStore from './store';
 import { ModalProvider } from './components/context/Modal_Context';
