0d32ca283b651edd32f5cbc3cb4fd154cb7ed505
Alejandro C. Grant <authorcgrant@gmail.com>
2022-02-09 16:06:05 -0800

    about to do a deep dive, safe checkpoint, looking into reformatting the process_search method

--- app/crawler/spider_lair/spiders/theraphosidae.py
+++ app/crawler/spider_lair/spiders/theraphosidae.py
@@ -26,18 +26,23 @@ class DeepCrawler1(scrapy.Spider):
                                 callback=self.process_search
                             )
             yield request
-        except: print(f'End of the line error for {self.name}.')
+        except: print(f'End of the line error in parse method for {self.name}.')
 
     def process_search(self, response):
         """Process search results.
         
         First result and last two results are omitted on this scrape.
-        Furthermore, results were forms so the link had to be parsed from
-        the `action` attribute as opposed to the `href`.
+        Furthermore, results were form elements so the link had to be
+        parsed from the `action` attribute as opposed to the `href` 
+        and the titles had pulled from the input values.
         """
-        forms = response.css('form::attr(action)')
-        print('hitting forms', type(forms))
-        i, res_length = 1, len(forms)
-        while i < (res_length - 2): 
-            yield { 'url': forms[i].get(), 'text': forms[i].get() }
-            i += 1
+        try:
+
+            links = response.css('form::attr(action)')
+            titles = response.css('input.SubmitLink')
+            print('hitting links', type(links))
+            i, res_length = 1, len(links)
+            while i < (res_length - 2): 
+                yield { 'url': links[i].get(), 'text': links[i].get() }
+                i += 1
+        except: print(f'End of the line error in process_search method for {self.name}.')
